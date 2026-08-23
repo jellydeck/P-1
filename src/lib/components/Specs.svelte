440957f24a9e27f5319f26fa02c1ca993b733479
Jaydip Sanghani <91427591+jellydeck@users.noreply.github.com>
2026-08-23 11:51:57 +0530

    specs design & groove svg updated

--- src/lib/components/Specs.svelte
+++ src/lib/components/Specs.svelte
@@ -6,19 +6,15 @@
 </script>
 
 <div class=" bg-gray text-black">
-	<div class="grid grid-cols-[1fr_2fr_2fr] border border-light-gray gap-px bg-light-gray *:bg-gray *:p-5">
-		<p class="text-(length:--step-2) bg-gray pl-2 col-start-1 row-start-1 row-span-4">P-I Specs</p>
-
+	<div class="grid grid-cols-2 md:grid-cols-[1fr_2fr_2fr] border border-light-gray gap-px bg-light-gray *:bg-gray *:p-4">
+	<p class="text-(length:--step-2) bg-gray pl-2 col-span-2 md:col-span-1 md:col-start-1 md:row-start-1 md:row-span-4">Specification</p>
 		<div class="flex items-center justify-center"><Numbers/> <Numbers start={1}/></div>
 		<p class="flex items-center">seven-segment display for crisp numbers</p>
-
 		<div class="flex items-center justify-center"><CoinCell class="w-50"/></div>
 		<p class="flex items-center">coin cell battery that lasts months</p>
-
-		<div class="flex items-center justify-center"><UniqueKeys class="w-60"/></div>
+		<div class="flex items-center justify-center"><UniqueKeys class="w-45"/></div>
 		<p class="flex items-center">distinct key shapes so your fingers can find the right key</p>
-
-		<div class="flex items-center justify-center"><Groove class="w-50"/></div>
+		<div class="flex items-center justify-center"><Groove class="w-45"/></div>
 		<p class="flex items-center">machine-drilled grooves for resting your fingers</p>
 	</div>
 </div>
